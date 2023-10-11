63e3a2a99768f02d29f563f64ce7f7914f872401
cyanbullets <86730474+cyanbullets@users.noreply.github.com>
2023-10-11 09:28:13 -0700

    REQUIRING READ_MEDIA_VIDEO INSTEAD OF FULL ACCESS!

--- trimcrop/src/main/java/com/video/trimcrop/PermActivity.kt
+++ trimcrop/src/main/java/com/video/trimcrop/PermActivity.kt
@@ -1,12 +1,9 @@
 package com.video.trimcrop
 
 import android.Manifest
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Build
-import android.os.Environment
-import android.provider.Settings
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -15,18 +12,20 @@ abstract class PermActivity : AppCompatActivity() {
 
     lateinit var doThis: () -> Unit
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     protected fun setupPermissions(doSomething: () -> Unit) {
         doThis = doSomething
         val permissionsToRequest = mutableListOf<String>()
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            if (!Environment.isExternalStorageManager()) {
-                val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
-                val uri = Uri.fromParts("package", packageName, null)
-                intent.data = uri
-                startActivity(intent)
-                return
-            }
+//            if (!Environment.isExternalStorageManager()) {
+//                val intent = Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION)
+//                val uri = Uri.fromParts("package", packageName, null)
+//                intent.data = uri
+//                startActivity(intent)
+//                return
+//            }
+            permissionsToRequest.add(Manifest.permission.READ_MEDIA_VIDEO)
         } else {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 permissionsToRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE)
